ef4e3d094ce1a610e5c7ee614d2d6d2f1e5d899e
sharkbot-neko <modoggu12345@outlook.com>
2026-01-16 18:15:50 +0900

    DAT_0107cab4を追加

--- Structs/Player/HomePlayer.hpp
+++ Structs/Player/HomePlayer.hpp
@@ -40,6 +40,7 @@ typedef struct HomePlayerBodyWork {
     char pad_0084[0x04];
 } HomePlayerBodyWork;
 
+// 0x0107CAC0
 typedef struct HomePlayer {
     DAT_00e3ee98* dat_00e3ee98;
     char pad_0000[0x56];
@@ -49,3 +50,15 @@ typedef struct HomePlayer {
     char pad_0001[0xDC4];
     HomePlayerBodyWork* playerbodywork;
 } HomePlayer;
+
+// 0x0107CAB4
+typedef struct DAT_0107cab4
+{
+    char pad_0000[0x04];
+    char pad_0001[0x04];
+    char pad_0002[0x04];
+    HomePlayer* player;
+    char pad_0003[0x04];
+    char pad_0004[0x04];
+    char pad_0005[0x04];
+};
